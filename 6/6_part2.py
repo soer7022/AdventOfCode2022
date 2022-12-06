0aa35e5ffac673c62ec7d14770f91b616f3b8d1c
Søren Eriksen <soeren.skov.eriksen@lego.com>
2022-12-06 07:30:32 +0100

    day 6

---
+++ 6/6_part2.py
@@ -0,0 +1,16 @@
+with open("input.txt") as file:
+    string_to_read = file.read().strip()
+
+for i in range(len(string_to_read)):
+    current_string = string_to_read[i:i+14]
+    seen = set()
+    found = False
+    print(current_string)
+    for char in current_string:
+        if char in seen:
+            found = True
+        else:
+            seen.add(char)
+    if not found:
+        print(i+14)
+        break
